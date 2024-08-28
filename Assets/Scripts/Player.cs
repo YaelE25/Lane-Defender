711441ba2da711b89f4a9df00dd5b6b4de9e9483
YaelE25 <157845181+YaelE25@users.noreply.github.com>
2024-08-28 10:16:40 -0500

    FirstPublish

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb2D;
+    [SerializeField] private float tankSpeed;
+    private float vertical;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        //run = true
+        vertical = context.ReadValue<Vector2>().y;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        rb2D.velocity= new Vector2(rb2D.velocity.x, vertical * tankSpeed);
+    }
+}
